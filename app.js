b93c7389f0a3d6418a69b37d436891ce58190742
Hasuuk3 <hasnain.khatri34@gmail.com>
2023-05-20 01:34:51 +0500

    a
    a

---
+++ app.js
@@ -0,0 +1,453 @@
+// //xxxxxxxxxxxxxx chapter 1 xxxxxxxxxxx
+// //----------------1
+// alert("Muhammad Hasnain")
+// alert("Usman Ghani")
+// alert("hasnain.khatri34@gmail.com")
+// alert("03362878112")
+// alert("******")
+// //------------2
+// alert("you are learning javascript")
+// //-------------3
+// alert("chapter 1 end")
+
+// //xxxxxxxxxxxxxxxxx chapter2 xxxxxxxxxxxx
+// //---------------1
+// var firstName;
+// //----------------2
+// var firstName;
+// firstName= "Hasnain"
+// //-----------------3
+// var teamName =" hasnain's team"
+// alert(teamName)
+// //-----------4
+// var bestMan = "Charlie";
+// bestMan="charlie2"
+
+// //xxxxxxxxxxxxxxx chapter3 xxxxxxxxxxxxx
+// //------------1
+// var caseQty;
+// //-----------------2
+// var caseQty=144;
+// //-------------3
+// var num="9"
+// //--------------4
+// var a=2;
+// var b=2;
+// console.log(a+b);
+// //-----------5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal)
+
+//----------6
+// var a=5;
+// a=5+3;
+// console.log(a) 
+
+//xxxxxxxxxxxxx chapter4 xxxxxxxxxxxxxxxxx
+//---------------------1
+// var productcost = 3.45;
+
+//--------------------2
+// var nameofBand;
+
+//--------------3
+// var legalname = 100;
+
+//----------------4
+// var firstName;
+// var lastName;
+// var combination = firstName + lastName;
+
+//-----------5
+//------------legal 
+// var thisIsfine;
+// var $noproblem;
+// var z5;
+// var Startwithcapital;
+//------------illegal
+// var 999;
+// var 39step;
+// var single word only;
+// var &No;
+
+// xxxxxxxxxxxxxxx chapter5 xxxxxxxxxxx
+//----------------------1
+//  var division = "/";
+//--------------------2
+// var num=20%6;
+// document.write(num)
+
+// xxxxxxxxxxxxxxxxxxxxxxx chapter  6 xxxxxxxxxxx
+
+// x = 7
+// x = x + 1; // 
+// x += 1;  //  answer
+// x++   //  answer
+
+
+// var x = 100;
+// x--
+// console.log(x);
+
+
+// var x = 50;
+// var y = x++; // answer is 50
+// console.log(y);
+// // but
+// var x = 50;
+// var y = ++x; // answer is 51
+// console.log(y);
+
+
+
+// var y = 50;
+// var z = --y;  //answer is 49
+// console.log(z);
+// //but
+// var y = 50;
+// var z = y--;  //answer is 50
+// console.log(z);
+
+
+// var num = 10;
+// var newNum = --num;
+// console .log(newNum)
+
+//  var num = 10;
+//  var newNum = ++num;
+//  console .log(newNum)
+
+
+// var x  = 20;
+// var y = ++x;
+// alert(y);
+
+// xxxxxxxxxxxxxxx chapter 7 xxxxxxxxxxxxxxxxx
+
+
+
+// var calculatedNum = 2 + (2 * 6);   //answer is 14
+// console.log(calculatedNum);
+
+// var calculatedNum = (2 + 2) * 6;   // answer is 24
+// console.log(calculatedNum);
+
+// var calculatedNum = (2 + 2) * (4 + 2);  // answer is 24
+// console.log(calculatedNum);
+
+
+// var calculatedNum = ((2 + 2) * 4) + 2;  //answer is 28
+// console.log(calculatedNum);
+
+
+
+// var calculatedNum = ( 2 + 2) *( 4 + 10);
+// console.log(calculatedNum);
+
+// var calculatedNum = 2 + (2 * 4) + 10;
+// console.log(calculatedNum);
+
+// var calculatedNum = (4 / 2 )* 4;   //is not posible
+// console.log(calculatedNum);
+
+
+// xxxxxxxxxxxxxxxx chapter 8 xxxxxxxxxxxxx
+
+// var num = "2" + "2";  // answer is 22
+// console.log(num);
+
+// var message = ("Hello," + "Dolly");
+// alert(message);
+
+
+
+// alert("33" + 3);  //answer is 333
+
+// var part1 = "Pakistan";
+// var part2 = "Zindabad";
+// alert(part1 + " " + part2);
+
+
+
+
+// var stringVariable = "Hello";
+// var numberVariable = 42;
+// var concatenatedValue = stringVariable + numberVariable;
+// console.log(concatenatedValue)
+
+
+//  var stringVariable = "Hello";
+//  var numberVariable = "world";
+//  var concatenatedValue = stringVariable +" "+ numberVariable;
+// alert(concatenatedValue)
+
+
+// xxxxxxxxxxx chapter 9 xxxxxxxxxxxxxxxxx
+
+// var firstName = prompt("Enter first name");
+// document.write(firstName)
+
+
+// var country = prompt("Enter Country name");
+// document.write(country)
+
+// var yourName = prompt("Enter Your Name");
+
+// var yourName = prompt("Enter Your Name" ,"name");
+// var defaultinput = yourName
+// document.write( defaultinput);
+
+// var  question = "What is your favorite food?";
+// var  defaultResponse = "Pizza";
+// var userResponse = prompt(question, defaultResponse);
+
+
+// var question = "What is your favorite food?";
+// var defaultResponse = "Pizza";
+// var userResponse = prompt(question, defaultResponse);
+// alert(userResponse);
+
+
+//  xxxxxxxxxxxxxxxx chapter 10 xxxxxxxxxxxxxx
+
+// var city = "Karachi"
+// if (city == "Karachi") {
+// console.log("The City OF Lights")
+// }
+
+
+// if (x === y) {
+//     var z = prompt("Enter the value of z?");
+// }
+
+// var zipcode = prompt("enter your zipcode")
+// var city =10010
+// if (zipcode==city ) {
+//     alert("karachi")
+// } else {
+//     alert("Please write correct zipcode")
+// } 
+
+// let x = 2; // The variable to test and potentially assign a new value
+
+// if (x === 1) {
+//   x = 3; // Assigning a new value to x if the condition is true
+// }
+
+// console.log(x); // Output the value of x to the console
+
+
+//  xxxxxxxxxxxxxxx chapter 11 xxxxxxxxxxxxxx
+
+// if (!(variable1 === variable2)) {
+//     // Rest of the code for the if statement
+//   }
+
+
+// if(variable1 >= variable2){
+
+// }
+
+
+// var x = 3; // The variable to test and potentially assign a new number
+// if (x !== 5) {
+//   x = 7; // Assigning a new number to x if the condition is true
+// }
+// console.log(x); // Output the value of x to the console
+
+
+
+// var num1 = 10;
+// var num2 = 20;
+// if (num1 !== num2) {
+//     alert("congratulations")
+// }
+
+
+// var enteredName = prompt("enter your first name")
+// if (enteredName !== "john"){
+//     alert("no match")
+// }
+// else{
+//     alert("name is match")
+// }
+
+// xxxxxxxxxxxxxxx chapter 12 xxxxxxxxxxxxx
+
+
+// var variable1 = 5;
+// var variable2 = 3;
+
+// if (variable1 >= variable2) {
+//   alert("Variable 1 is greater than or equal to Variable 2");
+// } else {
+//   alert("Variable 1 is less than Variable 2");
+// }
+
+
+
+
+// var perc = +prompt("enter your percentage")
+// if (perc <= 100 && perc >= 80) {
+//     document.write("your grade is A+")
+// }
+// else if (perc < 80 && perc >= 70) {
+//     document.write("your grade is A")
+// }
+// else if (perc < 70 && perc >= 60) {
+//     document.write("your grade is B")
+// }
+// else if (perc < 60 && perc >= 50) {
+//     document.write("your grade is C")
+// }
+// else if (perc < 50 && perc >= 40) {
+//     document.write("your grade is D")
+// }
+// else if (perc < 40 && perc >= 0) {
+//     document.write("fail")
+// }
+// else {
+//     document.write("your input is invalid")
+// }
+
+
+// var a = 10
+
+// if (a === 10) {
+//     alert("a is 10");
+//    } else {
+//     alert("The correct value of a is");
+//   }
+
+
+// var city = prompt("enter your city");
+
+// if (city == "karachi") {
+//     alert("karachi")
+// }
+
+// else if(city == "lahore"){
+// alert("lahore")
+// }
+// else{
+// alert("Is not correct city")
+// }
+
+
+//  xxxxxxxxxxxx chapter 13 xxxxxxxxxxxxx
+
+
+// if (a === b && c === d) {
+//     // Rest of the code for the if statement
+//   }
+
+// if (a === b || c !== d) {
+//     // Rest of the code for the if statement
+//   }
+
+// if ((nam === "Hamza" || nam === "Arsalan") && age < 60) {
+//     // Rest of the code for the if statement
+//   }
+
+
+// var num1 = 10;
+// var num2 = 20;
+// if (num1 < num2||num1>num2 ){
+// alert("thank you")
+// }
+
+// var firstname = "Huzaifa";
+// var secondname ="faheem";
+
+// var enterfirstname =prompt("enter your first name ")
+// var entersecondtname =prompt("enter your second name ")
+
+// if (firstname==enterfirstname||secondname==enterfirstname){
+//     alert("thank you")
+// }
+
+//  xxxxxxxxxxxxxxxx chapter 14 xxxxxxxxxxx
+
+
+// var Password = prompt("enter your password")
+// if (Password !== "") {
+//     if (Password.length <= 5 ) {
+// alert("password must be greater than 5")
+//     }
+//     else{
+//         alert("ok")
+//     }
+// }
+
+
+// var a=1;
+// var c="Max";
+// if (a === 1) {
+//     if (c === "Max") {
+//      alert("OK");
+//      }
+//     }
+
+
+
+// var a=1;
+// var c="Max";
+// if (a === 1 && c === "Max") {
+//     alert("OK");
+// }
+
+
+// var firstvariable = 10;
+// var secondvariable = 10;
+
+// if (firstvariable == secondvariable) {
+//     if (firstvariable <= secondvariable) {
+//         alert("okay")
+//     }
+
+// }
+
+
+
+//  xxxxxxxxxxxx chapter 15 xxxxxxxxxxxx
+
+
+// var ary = ["h","s","b","u","v","c"]
+
+
+// var  myArray = ["Hello, World!"];
+
+
+// var alphabet = ["h","i","j","k"]
+// document.write(alphabet[2])
+
+
+// var alphabet=["h","i","j","k", "l","m", "n", "o"]
+// console.log(alphabet.length);
+
+
+// let myArray = ["First element"];
+// myArray[1] = "Second element";
+
+// alert(myArray[1]);
+
+
+//  xxxxxxxxxxxxxxxxx chapter 16 xxxxxxxxxxxxxxxxx
+
+// var Alphabet = ["h", "i", "j", "k"]
+// Alphabet.push("L")
+// alert(Alphabet[4])
+
+
+
+// var Alphabet=["h","i","j","k"]
+// Alphabet.pop()
+// console.log(Alphabet);
+
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.push(123);
+
+// console.log(Alphabet);
